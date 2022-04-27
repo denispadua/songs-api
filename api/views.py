7776a0cdcc1fee55d8d86620378db3351e743810
Denis Padua <denisdepadua@outlook.com>
2022-04-27 11:48:21 -0300

    improved code readability and fixed redis delete cache

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 import json
+
 from flask import Blueprint, jsonify, request
+
 import api.services as service
-from database.db import redis
+
 api_bp = Blueprint('api_bp', __name__, url_prefix='/api')
 
 
@@ -14,23 +16,21 @@ def cache():
     transaction = service.search_transaction(artist_name)
 
     if transaction['Items'] != []:
-
         transaction_data = transaction['Items'][0]
 
         if transaction_data['Cache'] == 'True':
-
-            songs_cache = redis.get(transaction_data['Id'])
+            songs_cache = service.get_cache(transaction_data['Id'])
 
             if songs_cache:
                 if cache == 'True':
                     songs_cache = json.loads(songs_cache)
                     return jsonify({'cache': songs_cache})
                 else:
-                    redis.delete(artist_name)
+                    service.delete_cache(transaction_data['Id'])
 
 
 @api_bp.get('/')
-def get():
+def get_top_songs():
 
     artist_name = request.args.get('artist_name', type=str)
     cache = request.args.get('cache', 'True', type=str)
@@ -38,7 +38,7 @@ def get():
     artist_data = service.get_json_data('/search', params={'q': artist_name})
     artist_id = service.get_artist_id(artist_data, artist_name)
 
-    uri_songs = f'/artists/{artist_id}/songs'
+    uri_songs = '/artists/{}/songs'.format(artist_id)
     top_songs = service.get_json_data(
         uri_songs, params={'sort': 'popularity', 'per_page': 10})
 
