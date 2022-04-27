c2d546719cf40029ebdecfefa98eb2687c65bfc8
Denis Padua <denisdepadua@outlook.com>
2022-04-27 12:48:10 -0300

    moved db operations to db.py

--- database/db.py
+++ database/db.py
@@ -1,9 +1,9 @@
 import boto3
+from flask import abort
 from redis import Redis
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from boto3.dynamodb.conditions import Key
+from datetime import datetime, timedelta
 
 dynamodb = boto3.resource(
     'dynamodb',
@@ -13,3 +13,40 @@ dynamodb = boto3.resource(
 
 redis = Redis.from_url(
     os.getenv('REDIS_CONNECTION_STRING'), password=os.getenv('PASSWORD_REDIS'), decode_responses=True)
+
+
+def save_transaction(transaction):
+
+    try:
+        table = dynamodb.Table('Transactions')
+        table.put_item(Item=transaction)
+    except:
+        abort(500, description='Its not possible to create transaction')
+
+
+def search_transaction(artist_name):
+
+    table = dynamodb.Table('Transactions')
+
+    interval = int(datetime.timestamp(datetime.now()-timedelta(7)))
+
+    return table.query(
+        KeyConditionExpression=Key('Artist').eq(artist_name) &
+        Key('Time').gte(interval),
+        Limit=1,
+        ScanIndexForward=False)
+
+
+def create_cache(id, top_songs):
+
+    redis.setex(id, timedelta(7), top_songs)
+
+
+def get_cache(id):
+
+    return redis.get(id)
+
+
+def delete_cache(id):
+
+    redis.delete(id)
