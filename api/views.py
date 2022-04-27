4e23f5f27634b5c2b53ccdee901aa7f654a8065a
Denis Padua <denisdepadua@outlook.com>
2022-04-27 12:47:50 -0300

    import db operations

--- api/views.py
+++ api/views.py
@@ -3,12 +3,13 @@ import json
 from flask import Blueprint, abort, jsonify, request
 
 import api.services as service
+from database import db
 
 api_bp = Blueprint('api_bp', __name__, url_prefix='/api')
 
 
 @api_bp.before_request
-def cache():
+def get_cached_songs():
 
     artist_name = request.args.get('artist_name', type=str)
     cache = request.args.get('cache', 'True', type=str)
@@ -16,20 +17,20 @@ def cache():
     if artist_name is None:
         abort(400, "artist_name is required")
 
-    transaction = service.search_transaction(artist_name)
+    transaction = db.search_transaction(artist_name)
 
     if transaction['Items'] != []:
         transaction_data = transaction['Items'][0]
 
         if transaction_data['Cache'] == 'True':
-            songs_cache = service.get_cache(transaction_data['Id'])
+            songs_cache = db.get_cache(transaction_data['Id'])
 
             if songs_cache:
                 if cache == 'True':
                     songs_cache = json.loads(songs_cache)
                     return jsonify({'cache': songs_cache})
                 else:
-                    service.delete_cache(transaction_data['Id'])
+                    db.delete_cache(transaction_data['Id'])
 
 
 @api_bp.get('/')
@@ -46,11 +47,12 @@ def get_top_songs():
         uri_songs, params={'sort': 'popularity', 'per_page': 10})
 
     transaction = service.create_transaction(artist_name, cache)
-    service.save_transaction(transaction)
+    db.save_transaction(transaction)
 
     if cache == 'True':
-        service.create_cache(transaction['Id'],
-                             top_songs=top_songs['response']['songs'])
+        json_top_songs = json.dumps(top_songs['response']['songs'])
+        db.create_cache(transaction['Id'],
+                        top_songs=json_top_songs)
 
     return jsonify({
         'data': top_songs['response']['songs']
